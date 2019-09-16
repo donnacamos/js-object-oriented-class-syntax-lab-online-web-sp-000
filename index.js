3514ef60d6d2529c1166ad60c372d7707978a3d9
Donna Amos <donnacamos.developer@gmail.com>
2019-09-16 14:59:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ BoardMember.prototype.doCharity = function() {
 BoardMember.prototype.releasePressStatement = function() {
   console.log("You will see great things from Scuber.")
 }
+
+BoardMember.prototype.sayHi = function() {
+  console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)
+}
