901820cddd125c9ef550e9020af92eb45d21fc05
Donna Amos <donnacamos.developer@gmail.com>
2019-09-16 15:02:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,9 @@ BoardMember.prototype.releasePressStatement = function() {
 BoardMember.prototype.sayHi = function() {
   console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)
 }
+
+class Ceo extends BoardMember {
+  hireEmployee(){
+    console.log("Welcome aboard!")
+  }
+}
